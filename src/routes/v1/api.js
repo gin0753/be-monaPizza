1f8788f2ba3499277878276639f1386f04b6e56b
Gin Huang <ginrobin0204@gmail.com>
2020-12-04 13:58:51 +1000

    verifytoken

--- src/routes/v1/api.js
+++ src/routes/v1/api.js
@@ -30,7 +30,7 @@ const loginConfirmation = require("../../controllers/loginConfirmation");
 router.get("/menu/:name/:size", menuControllers.showOnePizza);
 router.post("/menu/:page/:pageSize", menuControllers.showBulkPizza);
 router.post("/menu", verifyToken, verifyRole('ROLE.ADMIN'), menuControllers.addPizza);
-router.put("/menu/:userId/:pizzaName", validateId, verifyRole('ROLE.ADMIN'), menuControllers.updatePizza);
+router.put("/menu/:userId/:pizzaName", verifyToken, verifyRole('ROLE.ADMIN'), menuControllers.updatePizza);
 router.delete("/menu/:userId/:pizzaName", verifyToken, verifyRole('ROLE.ADMIN'), menuControllers.deletePizza);
 
 
