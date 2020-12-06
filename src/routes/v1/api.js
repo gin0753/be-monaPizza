0c8e179c227da09d901f189ccb495ac89a9aff03
Gin Huang <ginrobin0204@gmail.com>
2020-12-06 12:59:36 +1000

    update order api

--- src/routes/v1/api.js
+++ src/routes/v1/api.js
@@ -76,6 +76,7 @@ router.get('/confirmation/:token', loginConfirmation.emailConfirm);
 router.get('/order/:id', orderControllers.displayOneOrder);
 router.get('/order/:userId/:page/:pageSize', orderControllers.displayClientOrder);
 router.post('/order', orderControllers.generateOrder);
+router.put('/order/:id', orderControllers.updateOneOrder)
 router.delete('/order/:id', orderControllers.deleteOrder);
 
 
