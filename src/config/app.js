76d67e0dc9a2ecc4b8ab26adf961c006dcc96b46
gin0753 <ginrobin0204@gmail.com>
2020-12-20 13:02:48 +1000

    src

--- src/config/app.js
+++ src/config/app.js
@@ -1,7 +1,7 @@
 process.env.NODE_ENV = process.env.NODE_ENV || "development";
 
 module.exports = {
-  port: process.env.PORT || 8000,
+  port: process.env.PORT || 'https://protected-scrubland-99331.herokuapp.com',
   api: {
     prefix: process.env.API_PREFIX || "/api/v1",
   },
